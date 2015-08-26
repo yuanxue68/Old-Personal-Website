645040a5b0297828e81393dae20c291748b3de9f
Yuan Xue <yuanxue68@gmail.com>
2015-08-25 23:20:44 -0400

    reverted partlejs intro to vegasjs intro

--- js/index.js
+++ js/index.js
@@ -10,42 +10,12 @@ $(function() {
 	});
 });
 
-jQuery(document).ready(function($){
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 300,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
-		//duration of the top scrolling animation (in ms)
-		scroll_top_duration = 700,
-		//grab the "back to top" link
-		$back_to_top = $('.cd-top');
-
-	//hide or show the "back to top" link
-	$(window).scroll(function(){
-		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) { 
-			$back_to_top.addClass('cd-fade-out');
-		}
-	});
-
-	//smooth scroll to top
-	$back_to_top.on('click', function(event){
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: 0 ,
-		 	}, scroll_top_duration
-		);
-	});
-
+$(document).ready(function() {
+	initScrollTop();
+	disableMobileAnim();
 });
 
-function scrollToTimeline() {
-	$("html,body").animate({
-	    'scrollTop':   $('#aboutme').offset().top
-	}, 700);
-}
-
-jQuery(document).ready(function() {
+function disableMobileAnim(){
 	if(! /iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase())) 
 	{
 	    jQuery('.introup').addClass("hidethis").viewportChecker({
@@ -79,6 +49,39 @@ jQuery(document).ready(function() {
 	   $(".title").removeClass("smalltitle");
 	   $(".arrow").removeClass("hidethis");
 	}
-});
+};
 
 
+function initScrollTop(){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+};
+
+function scrollToTimeline() {
+	$("html,body").animate({
+	    'scrollTop':   $('#aboutme').offset().top
+	}, 700);
+};
